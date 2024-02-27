c95f1631cc3921bc54c7d658e9c7cb7ac6f93e0e
kanyok <134153117+nullvoid-ky@users.noreply.github.com>
2024-02-27 07:45:40 +0700

    Create a_plus_b.cpp

---
+++ 0000_a_plus_b/a_plus_b.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define GAP " "
+#define EL "\n"
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << EL;
+    return 0;
+}
